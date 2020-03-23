1df48ef753997e4408df62ce6cecaa665f046935
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-23 15:30:58 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/task1.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class task1 {
+	 public static void main(String args[]){
+		 double a = 31.2;
+		 String b = "Java研修";
+		 int c = 9;
+		 long d = 2147483648L;
+		 char e = '才';
+
+	 }
+
+}
