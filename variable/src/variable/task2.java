b05cba555e766a51df178401c15f58cea004864d
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-23 16:41:24 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/task2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class task2 {
+	public static void main(String args[]){
+		String name = "幅下聖也";
+		final String TELPHONE_NUMBER ="444-111-222";
+
+		System.out.println(name + "です。");
+		System.out.print("私の携帯電話の番号は'" + TELPHONE_NUMBER +  "'です。");
+	}
+}
