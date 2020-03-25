70788002deb8c4352bd3408b6b3da914cc1a60f0
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-25 13:52:30 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/task2.java
@@ -0,0 +1,9 @@
+package calc;
+
+public class task2 {
+	public static void main(String args[]){
+		System.out.println("合計:" + (10 + 12 + 13));
+		System.out.println("平均(3で割った時):" + (10+12+13)/3);
+		System.out.println("平均(3.0で割った時):" + (10+12+13)/3.0);
+	}
+}
