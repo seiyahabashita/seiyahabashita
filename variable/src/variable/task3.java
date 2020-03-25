c5d99909cb9a3d0f99ec53adf0324212a50ccaf4
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-25 12:07:48 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/task3.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class task3 {
+	public static void main(String args[]){
+		int a = 219;
+		long b = a;
+		System.out.println(" long b の値: " + b );
+
+		double c = 364.2;
+		float d =(float) c;
+		System.out.println(" float d の値: " + d );
+
+		long e = 123456L;
+		double f = e;
+		System.out.println(" float f の値: " + f );
+	}
+}
