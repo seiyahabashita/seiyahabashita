4fce427b0a9e9cfe43800e205c5e9bb5cb856f33
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-27 15:43:46 +0900

    課題提出 - 条件分岐

---
+++ branch/src/branch/task5.java
@@ -0,0 +1,25 @@
+package branch;
+
+import java.util.Scanner;
+
+public class task5 {
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+
+		int numA = sc.nextInt();
+		int numB = sc.nextInt();
+		int numC = sc.nextInt();
+		System.out.println("numA:" + numA);
+		System.out.println("numB:" + numB);
+		System.out.println("numC:" + numC);
+		int max;
+
+		  max = numA;
+		  if(numB > max) max = numB;
+		  if(numC > max) max = numC;
+
+		  System.out.printf("最大値:"+ max);
+		  
+
+	}
+}
