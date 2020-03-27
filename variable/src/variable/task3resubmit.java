0e01b0a6d67ebe61b299ab02356544148d00f1d8
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-27 11:36:56 +0900

    再提出 -変数・定数

---
+++ variable/src/variable/task3resubmit.java
@@ -0,0 +1,24 @@
+ package variable;
+
+public class task3resubmit {
+	public static void main(String args[]){
+		int a = 219;
+		long b = a;
+
+
+		double c = 364.2;
+		float d =(float) c;
+
+
+		long e = 123456L;
+		double f = e;
+		System.out.println("変換前");
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(e);
+		System.out.println("変換後");
+		System.out.println( b );
+		System.out.println( d );
+		System.out.println( f );
+	}
+}
