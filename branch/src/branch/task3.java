c30ed7ad41762f5cf46c8cdc77b7909eeaa24066
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-27 12:30:31 +0900

    課題提出 - 条件分岐

---
+++ branch/src/branch/task3.java
@@ -0,0 +1,20 @@
+package branch;
+
+import java.util.Scanner;
+
+public class task3 {
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+		if(num > 0) {
+			System.out.println("入力された数字は正の数です");
+		}
+		if(num < 0) {
+			System.out.println("入力された数字は負の数です");
+		}
+		if(num == 0) {
+			System.out.println("入力された数字は0です");
+		}
+		}
+		}
