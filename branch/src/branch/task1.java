26413888f6b2cba54601cecf529257da20bfeabd
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-27 12:07:34 +0900

    課題提出 - 条件分岐

---
+++ branch/src/branch/task1.java
@@ -0,0 +1,15 @@
+package branch;
+
+public class task1 {
+	public static void main(String args[]){
+		int a =4;
+
+		System.out.println("10以上の数値を入力してください");
+		System.out.println(a);
+		if(a > 10)System.out.println("値:" + a);
+		if(a < 10) {
+			System.out.println("値:" + a*10);
+		}
+
+	}
+}
