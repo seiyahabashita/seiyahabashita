d0805d2e81bfb2ea06d5974e8c0ca462c8f83b2f
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-30 13:20:08 +0900

    「課題提出 - ループ

---
+++ loop/src/loop/task4.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.Scanner;
+
+public class task4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("正の整数を入力してください：");
+		int a = sc.nextInt();
+
+		System.out.print("約数:");
+		for(int i=1; i<=a; i++){
+			if(a % i == 0) {
+			System.out.print(i);
+			}
+			}
+	}
+			}
+
+
+
