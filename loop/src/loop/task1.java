5741a1d913176c6ee3bb507d41d6b0da4002517d
seiyahabashita <huyunoyuki1225@gmail.com>
2020-03-30 11:29:48 +0900

    課題提出 - ループ

---
+++ loop/src/loop/task1.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class task1 {
+	 public static void main(String[] args) {
+		 Scanner sc =new Scanner(System.in);
+
+			int num = sc.nextInt();
+			int sum = 0;
+	        int i = 0;
+	        while (i <= num) {
+	            sum += i;
+	            i++;
+	        }
+	        System.out.println("数字を入力してください:" + num);
+	        System.out.println("1～"+ num +"までの合計:"  + sum);
+	 }
+}
