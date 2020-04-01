258fdc85ab0f62f6dcd77dfe329629e874f3e46b
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-01 15:01:39 +0900

    再提出 - 四則演算

--- calc/src/calc/task2.java
+++ calc/src/calc/task2.java
@@ -2,8 +2,15 @@ package calc;
 
 public class task2 {
 	public static void main(String args[]){
-		System.out.println("合計:" + (10 + 12 + 13));
-		System.out.println("平均(3で割った時):" + (10+12+13)/3);
-		System.out.println("平均(3.0で割った時):" + (10+12+13)/3.0);
+		int num1;
+		num1 = 10;
+		int num2;
+		num2 = 12;
+		int num3;
+		num3 = 13;
+		
+		System.out.println("合計:" + (num1 + num2 + num3));
+		System.out.println("平均(3で割った時):" + (num1+num2+num3)/3);
+		System.out.println("平均(3.0で割った時):" + (num1+num2+num3)/3.0);
 	}
 }
