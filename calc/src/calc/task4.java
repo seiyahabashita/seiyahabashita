e72ea7893a1a73099e41ffcd5342174d56eb346a
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-01 11:26:45 +0900

    再提出 - 四則演算

--- calc/src/calc/task4.java
+++ calc/src/calc/task4.java
@@ -3,8 +3,8 @@ package calc;
 public class task4 {
 	public static void main(String args[]){
 		int num = 192837;
-		System.out.println("1の位を除く数:" + num / 10);
+		System.out.println("1の位を除く数: " + num / 10);
 
-		System.out.println("1の位の数:" + num % 10);
+		System.out.println("1の位の数: " + num % 10);
 	}
 }
