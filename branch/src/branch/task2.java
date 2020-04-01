a1d83e10c8106cd6d31e2219661acf99059c7343
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-01 15:56:03 +0900

    再提出 - 条件分岐

--- branch/src/branch/task2.java
+++ branch/src/branch/task2.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class task2 {
 	public static void main(String args[]){
 		Scanner sc =new Scanner(System.in);
-		System.out.println("10以上の数値を入力してください");
+		System.out.println("10以上の数字を入力してください");
 		int num = sc.nextInt();
 		System.out.println(num);
 		if(num > 10) {
-		System.out.println("値:"+ num);
+		    System.out.println("値:"+ num);
 		}
-		if(num < 10) {
+		else if(num < 10) {
 			System.out.println("値:0" + num);
 		}
-	} 
+	}
 }
