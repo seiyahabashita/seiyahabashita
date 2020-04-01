3a06f9d0f1694b624f5bfeb9d696a9239fab8958
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-01 14:58:49 +0900

    再提出 - 四則演算

--- calc/src/calc/task1.java
+++ calc/src/calc/task1.java
@@ -2,8 +2,9 @@ package calc;
 
 public class task1 {
 	public static void main(String args[]){
-		System.out.println((3+2)*6+5);
+		System.out.println("修正前: " + (3+2*6+5));
+		System.out.println("修正後: " + ((3+2)*6+5));
 
-		System.out.println("24%3の余りは" + 24%3);
+		System.out.println("25/3の商" + 25/3 +": 余り" +  25%3);
 	}
 }
