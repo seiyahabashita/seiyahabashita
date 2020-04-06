d0bd7f5087960ac31cbb8f92c78e9b164c3026b1
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-06 14:05:40 +0900

    課題提出 - 配列

---
+++ array/src/array/task2.java
@@ -0,0 +1,16 @@
+package array;
+
+public class task2 {
+	public static void main(String[] args) {
+		int[] array = new int[8];
+		for(int i = 1; i <= 7; i++) {
+			array[i] = i + 0;
+			if(i<7) {
+				System.out.print(i + ",");
+			}else{
+				System.out.println(i);
+			}
+		}
+	}
+}
+
