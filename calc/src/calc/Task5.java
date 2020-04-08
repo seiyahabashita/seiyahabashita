6079b23c496072ba8732ae5a9bb648273e466726
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-08 11:15:39 +0900

    再提出 - 四則演算

--- calc/src/calc/Task5.java
+++ calc/src/calc/Task5.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Task5 {
 	public static void main(String args[]){
 		Scanner sc =new Scanner(System.in);
-		String star;star ="ノートPCの値段: ";
-		int price; price = 89800;
-		String en; en = "円";
+		String star ="ノートPCの値段: ";
+		int price = 89800;
+		char en = "円";
 		System.out.println(star + price + en);
 		String line1 = sc.nextLine();
 		String line2 = sc.nextLine();
