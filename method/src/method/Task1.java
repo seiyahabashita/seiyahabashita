e968eebb5f2784bd1f40ca5af7bc048f35bbd197
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-10 15:53:42 +0900

    課題提出 - メソッド

---
+++ method/src/method/Task1.java
@@ -0,0 +1,24 @@
+package method;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		name();
+		old(23);
+		height(172.0);
+		System.out.println("よろしくお願いします。");
+	}
+	public static void name() {
+		System.out.println("私の名前は幅下聖也です。");
+	}
+	public static void old(int num) {
+		System.out.println("年齢は" + num + "歳です。");
+	}
+	public static void height(double num2) {
+		System.out.println("身長は" + num2 + "cmです。");
+	}
+
+
+}
+
+
--- vrietyArray/src/vrietyArray/Task2.java
+++ vrietyArray/src/vrietyArray/Task2.java
@@ -2,14 +2,13 @@ package vrietyArray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Task2 {
 	public static void main(String[] args) {
 
 		Integer[] numArray = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60};
-		List listOdd = new ArrayList();
-		List listEven = new ArrayList();
+		ArrayList listOdd = new ArrayList();
+		ArrayList listEven = new ArrayList();
 
 		for (int num : numArray) {
 			if (num % 2 == 0) {
