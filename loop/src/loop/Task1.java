c213f4eb9ca4d63738088a1d4a393860110c3fac
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-10 14:16:13 +0900

    再提出 - ループ

--- loop/src/loop/Task1.java
+++ loop/src/loop/Task1.java
@@ -3,17 +3,17 @@ package loop;
 import java.util.Scanner;
 
 public class Task1 {
-	 public static void main(String[] args) {
-		 Scanner sc =new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
 
-			int num = sc.nextInt();
-			int sum = 0;
-	        int i = 0;
-	        while (i <= num) {
-	        sum += i;
-	        i++;
-	        }
-	        System.out.println("数字を入力してください: " + num);
-	        System.out.println("1～"+ num +"までの合計: "  + sum);
-	 }
+		int num = sc.nextInt();
+		int sum = 0;
+		int i = 0;
+		while (i <= num) {
+			sum += i;
+			i++;
+		}
+		System.out.println("数字を入力してください: " + num);
+		System.out.println("1～"+ num +"までの合計: "  + sum);
+	}
 }
