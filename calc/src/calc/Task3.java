e72afb55f7c11aba43bfd22be3f1ee051843855d
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-13 11:09:50 +0900

    再提出 - 四則演算

--- calc/src/calc/Task3.java
+++ calc/src/calc/Task3.java
@@ -9,10 +9,14 @@ public class Task3 {
 		int num2 = sc.nextInt();
 		int num3 = sc.nextInt();
 		int S;
+		int S2;
+		double S3;
 		S = num1 + num2 + num3;
+		S2 = (num1 + num2 + num3) /3;
+		S3 = (num1 + num2 + num3) /3.0;
 		System.out.println("合計: " + (S));
-		System.out.println("平均(3で割った時): " + (S) /3);
-		System.out.println("平均(3.0で割った時): " + (S) /3.0);
+		System.out.println("平均(3で割った時): " + (S2));
+		System.out.println("平均(3.0で割った時): " + (S3));
 		sc.close();
 	}
 }
