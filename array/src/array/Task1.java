43033859daeeb6f75bbf8a764a9852bf08cc2de7
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-13 16:01:08 +0900

    再提出 - 配列

--- array/src/array/Task1.java
+++ array/src/array/Task1.java
@@ -2,12 +2,17 @@ package array;
 
 public class Task1 {
 	public static void main( String[] args ) {
-        int sum = 0;
-        int[] a1 = { 4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
-        for( int data : a1) {
-                sum += data;
-        }
-        System.out.println("配列:" + a1);
-        System.out.println("合計:" + sum);
-}
+		int[] array = new int[] { 4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
+		System.out.print("配列:[");
+		int sum = 0;
+		for( int i = 0; i < array.length ; i++) {
+			sum +=array[i];
+			if(array[i] > array[array.length -1]){
+				System.out.print(array[i] + ",");
+			}else {
+				System.out.println(array[array.length -1] +"]");
+			}
+		}
+		System.out.println("合計:" + sum);
+	}
 }
