a5928fc07fbda094a507d45263f499cc32f02ee1
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-15 10:52:14 +0900

    再提出 - ループ

--- loop/src/loop/Task3.java
+++ loop/src/loop/Task3.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 public class Task3 {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 4212f16f425e4293891ce8b6cb418fceadd700dd
 		System.out.print("数字を入力してください：");
 		int n = sc.nextInt();
 
