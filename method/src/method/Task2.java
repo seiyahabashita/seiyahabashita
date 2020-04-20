858f50f751fb2c59a839832eb8469d082daefb9b
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-20 17:14:16 +0900

    再提出 - メソッド

--- method/src/method/Task2.java
+++ method/src/method/Task2.java
@@ -2,11 +2,11 @@ package method;
 
 public class Task2 {
 	public static void main(String[] args) {
-		double s = add(4,3);
+		double s = getTriangleArea(4,3);
 		System.out.println("三角形の面積");
 		System.out.println(s);
 	}
-	private static double add(double num1, double num2) {
+	private static double getTriangleArea(double num1, double num2) {
 		double Ans = (num1 * num2)/2;
 		return Ans;
 	}
