3e06a7ea771c4f9e380d062faebc34dcc5334896
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-22 10:12:00 +0900

    再提出 - いろいろな配列

--- vrietyArray/src/vrietyArray/Task2.java
+++ vrietyArray/src/vrietyArray/Task2.java
@@ -7,17 +7,17 @@ public class Task2 {
 	public static void main(String[] args) {
 
 		Integer[] data = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60};
-		ArrayList<Integer> Odd = new ArrayList<>();
-		ArrayList<Integer> Even = new ArrayList<>();
+		ArrayList<Integer> odd = new ArrayList<>();
+		ArrayList<Integer> even = new ArrayList<>();
 
 		for (int num : data) {
 			if (num % 2 == 0) {
-				Even.add(num);
+				even.add(num);
 			} else {
-				Odd.add(num);
+				odd.add(num);
 			}
 		}
-		System.out.println("奇数：" + Arrays.toString(Odd.toArray()) + "\r\n"
-				+ "偶数：" + Arrays.toString(Even.toArray()));
+		System.out.println("奇数：" + Arrays.toString(odd.toArray()) + "\r\n"
+				+ "偶数：" + Arrays.toString(even.toArray()));
 	}
 }
