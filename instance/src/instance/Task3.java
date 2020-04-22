f854e0a820424983f35b7a195386cc7a32261971
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-22 14:58:03 +0900

    課題提出 - インスタンス＆クラス

---
+++ instance/src/instance/Task3.java
@@ -0,0 +1,13 @@
+package instance;
+
+public class Task3 {
+	public static void main(String[] args) {
+		Person habashita = new Person();
+		habashita.name = "幅下聖也";
+		habashita.age = 23;
+
+		System.out.println("私の名前は" + habashita.name + "です。");
+		System.out.println("年齢は" + habashita.age + "です。");
+		System.out.println("よろしくお願いします。");
+	}
+}
