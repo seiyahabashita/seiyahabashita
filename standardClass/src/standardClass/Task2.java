406f93815b589698a2d807a936fa7c1b593a2105
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-22 17:56:19 +0900

    課題提出 - 標準クラス

---
+++ standardClass/src/standardClass/Task2.java
@@ -0,0 +1,12 @@
+package standardClass;
+
+public class Task2 {
+	public static void main(String[] args) {
+		String str = "Java Programming Traning";
+		String substr =  str.substring(17,24);
+		String substr2 = str.substring(0,16);
+		
+		System.out.println(substr);
+		System.out.println(substr2);
+	}
+}
