a551c6b74df03e9f0cc0bccd902ed66b46f32e96
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-29 14:44:18 +0900

    再提出 - メソッド

--- method/src/method/Task1.java
+++ method/src/method/Task1.java
@@ -3,15 +3,15 @@ package method;
 public class Task1 {
 
 	public static void main(String[] args) {
-		String name = ("幅下聖也");
+		String name = "幅下聖也";
 		int old = 23;
 		double height = 172.0;
 		myProfile(name, old, height);
 		System.out.println("よろしくお願いします。");
 	}
-	public static void myProfile(String str, int i, double d ) {
+	public static void myProfile(String str, int name, double age ) {
 		System.out.println("私の名前は" + str + "です。");
-		System.out.println("年齢は" + i + "歳です。");
-		System.out.println("身長は" + d + "cmです。");
+		System.out.println("年齢は" + name + "歳です。");
+		System.out.println("身長は" + age + "cmです。");
 	}
 }
