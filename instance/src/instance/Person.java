55624fd99d6da9a304961838d75918debdc81d15
seiyahabashita <huyunoyuki1225@gmail.com>
2020-04-29 15:11:29 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -6,8 +6,8 @@ public class Person {
 	public int age;
 	//操作(課題3で追加）
 	public void myProfile() {
-	System.out.println("私の名前は" + this.name + "です。");
-	System.out.println("年齢は" + this.age + "です。");
-	System.out.println("よろしくお願いします。");
+		System.out.println("私の名前は" + this.name + "です。");
+		System.out.println("年齢は" + this.age + "です。");
+		System.out.println("よろしくお願いします。");
 	}
 }
