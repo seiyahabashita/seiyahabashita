e2bfbc6f033768e98af373d4dfa2e35a551b23c1
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-01 11:02:43 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task4.java
+++ standardClass/src/standardClass/Task4.java
@@ -6,12 +6,12 @@ public class Task4 {
 	public static void main(String[] args) {
 		String hoge = "ABCDGOPQRSYZ";
 
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		String num = sc.nextLine();
 		sc.close();
 
 		System.out.println("探したい文字: " + num);
-		if(hoge.indexOf(num)>= 0) {
+		if(hoge.indexOf(num) >= 0) {
 			System.out.println(hoge +"は" + num + "を含む");
 		}else {
 			System.out.println(hoge +"は" + num + "を含まない");
