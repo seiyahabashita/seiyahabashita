b61a722979a7a1d499b9c96629edc64f20f40849
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-01 10:08:15 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task1.java
+++ standardClass/src/standardClass/Task1.java
@@ -3,8 +3,7 @@ package standardClass;
 public class Task1 {
 	public static void main(String[] args) {
 		String str = " tech-compass ";
-		String substr =  str.substring(1,13);
 		System.out.println("変換前: " + str);
-		System.out.println("変換後: " + substr.toUpperCase());
+		System.out.println("変換後: " + str.trim() .toUpperCase());
 	}
 }
