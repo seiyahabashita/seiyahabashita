7ca5f830f2e50ec4598f12d83e4adbed2fd8c8d9
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-04 14:31:12 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task3.java
+++ standardClass/src/standardClass/Task3.java
@@ -2,11 +2,12 @@ package standardClass;
 
 public class Task3 {
 	public static void main(String[] args) {
-		String hoge1 = "ABCDEFG";
-		StringBuffer str = new StringBuffer(hoge1);
-		String hoge2 = str.reverse().toString();
-
-		System.out.println("文字列: " + hoge1);
-		System.out.println(hoge2 + ": 列字文");
+		String str = "ABCDEFG";
+		System.out.println("文字列: " + str);
+		for(int i = str.length() - 1; i >= 0; i--) {
+			str = "GFEDCBA";
+		}
+		System.out.println(str + " :列字文");
 	}
 }
+
