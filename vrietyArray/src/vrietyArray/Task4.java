07ccb2f2f0ed5ff6877375db2ec6dea17db19f96
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-04 09:02:38 +0900

    再提出 - いろいろな配列

--- vrietyArray/src/vrietyArray/Task4.java
+++ vrietyArray/src/vrietyArray/Task4.java
@@ -1,27 +1,25 @@
 package vrietyArray;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Task4 {
 	public static void main(String[] args) {
-		HashMap<String,ArrayList<Integer>>lavel = new HashMap<>();
+		ArrayList <Integer>numbers = new ArrayList<>();
+		ArrayList <Integer>number2 = new ArrayList<>();
+		HashMap<String,ArrayList<Integer>>even_odd = new HashMap<>();
 
-		Integer[] numarray = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60, 29, 5, 46, 74, 14, 193, 215, 83};
-		ArrayList <Integer>listodd = new ArrayList<>();
-		ArrayList <Integer>listeven = new ArrayList<>();
+		even_odd.put("偶数",numbers);
+		even_odd.put("奇数",number2);
 
-		for (int num : numarray) {
-			if (num % 2 == 0) {
-				listeven.add(num);
+		int[] num = {21, 3, 32, 6, 99, 72, 78, 51, 1, 26, 87, 11, 48, 60, 29, 5, 46, 74, 14, 193, 215, 83};
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] % 2 == 0) {
+				numbers.add(num[i]);
 			} else {
-				listodd.add(num);
+				number2.add(num[i]);
 			}
 		}
-
-		System.out.println("奇数：" + Arrays.toString(listodd.toArray()) + "\r\n"
-				+ "偶数：" + Arrays.toString(listeven.toArray()));
+		System.out.println(even_odd);
 	}
-
 }
