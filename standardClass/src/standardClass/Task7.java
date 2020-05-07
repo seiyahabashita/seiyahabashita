d9775c170499bd81cca53efc69e05f1f54586991
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-07 12:00:48 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task7.java
+++ standardClass/src/standardClass/Task7.java
@@ -1,32 +1,26 @@
 package standardClass;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Task7 {
 	public static void main(String[] args) {
 		String[] week_name = {"日", "月", "火", "水", "木", "金", "土"};
-
 		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
 
-		System.out.print("現在日時: ");
-		System.out.print(cal.get(Calendar.YEAR) + "年");
-		System.out.print(cal.get(Calendar.MONTH) + 1 + "月");
-		System.out.println(cal.get(Calendar.DAY_OF_MONTH) + "日");
+
+		System.out.println("現在日時: " + sdf.format(cal.getTime()));
 
 		cal.add(Calendar.DAY_OF_MONTH, 7);
 
-		System.out.print("一週間後: ");
-		System.out.print(cal.get(Calendar.YEAR) + "年");
-		System.out.print(cal.get(Calendar.MONTH) + 1 + "月");
-		System.out.println(cal.get(Calendar.DAY_OF_MONTH) + "日");
+		System.out.println("一週間後: " + sdf.format(cal.getTime()));
 
-
-		cal.set(2021,5,11);
+		cal.set(2021,5,14);
+		cal.add(Calendar.DAY_OF_MONTH, -7);
+		cal.add(Calendar.MONTH, -1);
 		int week = cal.get(Calendar.DAY_OF_WEEK) - 1;
-		System.out.print("一年後: ");
-		System.out.print(cal.get(Calendar.YEAR) + "年");
-		System.out.print(cal.get(Calendar.MONTH) + "月");
-		System.out.print(cal.get(Calendar.DAY_OF_MONTH) -7 + "日");
-		System.out.println("(" + week_name[week] + ")");
+
+		System.out.println("一年後　: " + sdf.format(cal.getTime()) +"(" + week_name[week] + ")");
 	}
 }
