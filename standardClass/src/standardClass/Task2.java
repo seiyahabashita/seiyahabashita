179e6d39e19fff967b01ef04cafed105b439905f
seiyahabashita <huyunoyuki1225@gmail.com>
2020-05-11 16:15:50 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task2.java
+++ standardClass/src/standardClass/Task2.java
@@ -3,7 +3,7 @@ package standardClass;
 public class Task2 {
 	public static void main(String[] args) {
 		String str = "Java Programming Traning";
-		 System.out.println(str.substring(17,24));
+		 System.out.println(str.substring(17));
 		 System.out.println(str.substring(0,16));
 	}
 }
